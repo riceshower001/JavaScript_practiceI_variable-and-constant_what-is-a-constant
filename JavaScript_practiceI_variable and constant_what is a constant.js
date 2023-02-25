25318087360f8dc9834cc48baa7c29476ba17707
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 19:37:32 +0900

    Add files via upload

---
+++ JavaScript_practiceI_variable and constant_what is a constant.js
@@ -0,0 +1,8 @@
+// Please define the constant "language".
+const language = "French" ;
+
+// Please output the value of the constant "language".
+console.log(language) ;
+
+// Use the constant "language" to output, "I can speak *** (= language)."
+console.log("I can speak " + language + ".") ;
